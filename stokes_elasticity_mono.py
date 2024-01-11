7f56c2f5d7f1f8423d36ea59054db7560d63e480
Matthew Hennessy <matthew.hennessy@bristol.ac.uk>
2024-01-11 13:29:19 +0000

    update and comment monolithic code

--- stokes_elasticity_mono.py
+++ stokes_elasticity_mono.py
@@ -1,46 +1,84 @@
+"""
+This is a monolithic solver for pressure-driven flow around a
+neo-Hookean sphere.  The problem is formulated using the ALE 
+method, which maps the deformed geometry to the initial
+(undeformed) geometry.  The problem is solved using the
+initial geometry; the deformed geometry can be seen in
+Paraview
+
+The problem uses Lagrange multipliers to ensure the centre of
+mass of the particle remains fixed as well as to impose
+continuity of traction
+
+The code works by initially solving the problem at a small
+value of epsilon (ratio of fluid stress to elastic stiffness)
+and then gradually ramping up epsilon.  If convergence is
+not obtained then the code tries again using a smaller value
+of epsilon.
+
+"""
+
 from dolfin import *
 from multiphenics import *
 from helpers import *
-import numpy as np
 
+
+#---------------------------------------------------------------------
+# Setting up file names and paramerers
+#---------------------------------------------------------------------
+
+"""
+Define file names
+"""
+
+# name of the mesh found in mesh/
 meshname = 'channel_sphere'
-dir = '/media/eg21388/data/fenics/stokes_elasticity/'
 
-"""
-    parameters
-"""
+# directory for file output
+# dir = '/media/eg21388/data/fenics/stokes_elasticity/'
+dir = '/home/matt/data/fenics/stokes_elasticity/'
 
-eps_conv = 1e-2
-eps_max = 1
-de = 0.3
-de_min = 1e-3
-de_max = 1e-1
-
-# N = 1
-# e_all = [1e-1]
-
-eps = Constant(eps_conv)
-
-
-snes_solver_parameters = {"snes_solver": {"linear_solver": "mumps",
-                                          "maximum_iterations": 8,
-                                          "report": True,
-                                          "absolute_tolerance": 1e-8,
-                                          "error_on_nonconvergence": False}}
-
-parameters["ghost_mode"] = "shared_facet"
-
-
+# files for the fluid (f) and solid (s)
 output_f = XDMFFile(dir + "fluid.xdmf")
 output_f.parameters["rewrite_function_mesh"] = False
 output_f.parameters["functions_share_mesh"] = True
 output_f.parameters["flush_output"] = True
 
-
 output_s = XDMFFile(dir + "solid.xdmf")
 output_s.parameters["rewrite_function_mesh"] = False
 output_s.parameters["functions_share_mesh"] = True
 output_s.parameters["flush_output"] = True
+
+
+"""
+    Physical parameters
+"""
+
+# the initial value of epsilon to try solving the problem with
+eps_try = 0
+eps = Constant(eps_try)
+
+# the max value of epsilon
+eps_max = 1
+
+# the increment in epsilon to use if the code converges
+de = 0.05
+
+# the min and max values of the increments to make.
+de_min = 1e-3
+de_max = 1e-1
+
+
+"""
+Solver parameters
+"""
+snes_solver_parameters = {"snes_solver": {"linear_solver": "mumps",
+                                          "maximum_iterations": 20,
+                                          "report": True,
+                                          "absolute_tolerance": 1e-8,
+                                          "error_on_nonconvergence": False}}
+
+parameters["ghost_mode"] = "shared_facet"
 
 #---------------------------------------------------------------------
 # problem geometry: mesh and boundaries
@@ -81,93 +119,179 @@ P1 = FiniteElement("CG", mesh.ufl_cell(), 1)
 DGT = VectorElement("DGT", mesh.ufl_cell(), 1)
 P0 = FiniteElement("R", mesh.ufl_cell(), 0)
 
-# u_f, p_f, f_0, lam_p, u_s, p_s, lam, U_0, u_a, lam_a
-mixed_element = BlockElement(P2, P1, P0, P0, P2, P1, DGT, P0, P2, DGT)
-V = BlockFunctionSpace(mesh, mixed_element, restrict = [Of, Of, Of, Of, Os, Os, Sig, Sig, Of, Sig])
+"""
+Setting up the elements and solution: here is the notation:
+
+u_f: fluid velocity from Stokes equations
+p_f: fluid pressure from Stokes equations
+
+u_s: solid displacement from nonlinear elasticity
+p_s: solid pressure from nonlinear elasticity
+
+f_0: Lagrange multiplier corresponding to the force needed to pin the
+solid in place 
+
+U_0: the translational velocity of the solid
+
+lam: Lagrange multiplier corresponding to the fluid traction 
+acting on the solid
+
+lam_p: Lagrange multiplier to ensure the mean fluid pressure is zero
+
+u_a: fluid "displacement" from the ALE method (e.g. how to deform
+the fluid geometry)
+
+lam_a: Lagrange multiplier to ensure continuity of fluid and solid
+displacement (ensures compatibility between fluid/solid domains)
+
+"""
+
+mixed_element = BlockElement(P2, P1, P2, P1, P0, P0, DGT, P0, P2, DGT)
+V = BlockFunctionSpace(mesh, mixed_element, restrict = [Of, Of, Os, Os, Os, Of, Sig, Of, Of, Sig])
+
+X = BlockFunction(V)
+(u_f, p_f, u_s, p_s, f_0, U_0, lam, lam_p, u_a, lam_a) = block_split(X)
+
+
 
 # unknowns and test functions
 Y = BlockTestFunction(V)
-(v_f, q_f, g_0, eta_p, v_s, q_s, eta, V_0, v_a, eta_a) = block_split(Y)
+(v_f, q_f, v_s, q_s, g_0, V_0, eta, eta_p, v_a, eta_a) = block_split(Y)
 
 Xt = BlockTrialFunction(V)
 
-X = BlockFunction(V)
-(u_f, p_f, f_0, lam_p, u_s, p_s, lam, U_0, u_a, lam_a) = block_split(X)
-
+# Placeholder for the last converged solution
 X_old = BlockFunction(V)
+
 
 #---------------------------------------------------------------------
 # boundary conditions
 #---------------------------------------------------------------------
 
+"""
+Physical boundary conditions
+"""
+
+# Far-field fluid velocity
 far_field = Expression(('(1 - x[1] * x[1] / 0.25) * t', '0'), degree=0, t = 1)
 
+# impose the far-field fluid velocity upstream and downstream
 bc_inlet = DirichletBC(V.sub(0), far_field, bdry, inlet)
 bc_outlet = DirichletBC(V.sub(0), far_field, bdry, outlet)
+
+# impose the fluid pressure the outlet
 # bc_outlet = DirichletBC(V.sub(1), Constant(0), bdry, outlet)
+
+# impose no vertical fluid flow at the centreline axis
 bc_fluid_axis = DirichletBC(V.sub(0).sub(1), Constant(0), bdry, fluid_axis)
+
+# impose no-slip and no-penetration at the channel wall
 bc_wall = DirichletBC(V.sub(0), Constant((0, 0)), bdry, wall)
 
-bc_solid_axis = DirichletBC(V.sub(4).sub(1), Constant(0), bdry, solid_axis)
+# impose zero vertical solid displacement at the centreline axis
+bc_solid_axis = DirichletBC(V.sub(2).sub(1), Constant(0), bdry, solid_axis)
 
-ac_inlet = DirichletBC(V.sub(8), Constant((0,0)), bdry, inlet)
-ac_outlet = DirichletBC(V.sub(8), Constant((0,0)), bdry, outlet)
+"""
+Boundary conditions for the ALE problem for fluid 
+displacement.  These are no normal displacements
+"""
+
+ac_inlet = DirichletBC(V.sub(8).sub(0), Constant((0)), bdry, inlet)
+ac_outlet = DirichletBC(V.sub(8).sub(0), Constant((0)), bdry, outlet)
 ac_fluid_axis = DirichletBC(V.sub(8).sub(1), Constant((0)), bdry, fluid_axis)
 ac_wall = DirichletBC(V.sub(8).sub(1), Constant((0)), bdry, wall)
 
+
+# Combine all BCs together
 bcs = BlockDirichletBC([bc_inlet, bc_outlet, bc_fluid_axis, bc_wall, bc_solid_axis, ac_inlet, ac_outlet, ac_fluid_axis, ac_wall])
 
 
 #---------------------------------------------------------------------
-# Define kinematics
+# Define the model
 #---------------------------------------------------------------------
 
 I = Identity(2)
 
-# fluids
-F_a = I + grad(u_a)
-H_a = inv(F_a.T)
-J_a = det(F_a)
-
-sigma_f = J_a * (-p_f * I + grad(u_f) * H_a.T + H_a * grad(u_f).T) * H_a
-ic_f = div(J_a * inv(F_a) * u_f)
-
-# sigma_a = div(u_a) * I + grad(u_a) + grad(u_a).T
-
-E_a = 0.5 * (F_a.T * F_a - I)
-sigma_a = tr(E_a) * I + 2 * E_a / eps
-
-# solids
-# F_old = I + grad(u_s_old)
-# H_old = inv(F_old.T)
-
+"""
+Solids problem
+"""
+# deformation gradient tensor
 F = I + grad(u_s)
 H = inv(F.T)
+
+# (non-dim) PK1 stress tensor and incompressibility condition
 sigma_s = 1 / eps * (F - H) - p_s * H
 ic_s = det(F) - 1
 
-# sigma_s = -p_s * I + (grad(u_s) + grad(u_s).T) / eps
-# ic_s = div(u_s)
+"""
+Fluids problem: mapping the current configuration to the 
+initial configuration leads a different form of the
+incompressible Stokes equations
+"""
+
+# Deformation gradient for the fluid
+F_a = I + grad(u_a)
+H_a = inv(F_a.T)
+
+# Jacobian for the fluid
+J_a = det(F_a)
+
+# stress tensor and incompressibility condition for the fluid
+sigma_f = J_a * (-p_f * I + grad(u_f) * H_a.T + H_a * grad(u_f).T) * H_a
+ic_f = div(J_a * inv(F_a) * u_f)
+
+
+"""
+ALE problem
+"""
+
+# Laplace
+sigma_a = grad(u_a)
+
+# linear elasticity
+# nu_a = Constant(0.3)
+# E_a = 0.5 * (grad(u_a) + grad(u_a).T)
+# sigma_a = nu_a / (1 + nu_a) / (1 - 2 * nu_a) * tr(E_a) * I + 1 / (1 + nu_a) * E_a
+
+# nonlinear elasticity
+# nu_a = Constant(0.3)
+# E_a = 0.5 * (F_a.T * F_a - I)
+# sigma_a = F_a * (nu_a / (1 + nu_a) / (1 - 2 * nu_a) * tr(E_a) * I + 1 / (1 + nu_a) * E_a)
+
 
 #---------------------------------------------------------------------
 # build equations
 #---------------------------------------------------------------------
 ez = as_vector([1,0])
 
-
+# Stokes equations for the fluid
 FUN1 = -inner(sigma_f, grad(v_f)) * dx(fluid) + inner(lam("+"), v_f("+")) * dS
+
+# Incompressibility for the fluid
 FUN2 = ic_f * q_f * dx(fluid) + lam_p * q_f * dx(fluid)
 
+# Nonlinear elasticity for the solid
 FUN3 = -inner(sigma_s, grad(v_s)) * dx(solid) + inner(as_vector([f_0, 0]), v_s) * dx(solid) - inner(lam("-"), v_s("-")) * dS
+
+# Incompressibility for the solid
 FUN4 = ic_s * q_s * dx(solid)
 
+# Continuity of fluid velocity at the solid
 FUN5 = inner(avg(eta), u_f("+") - as_vector([U_0("+"), 0])) * dS
-FUN6 = dot(as_vector([1, 0]), lam("+")) * V_0("+") * dS
 
+# No total axial traction on the solid (ez . sigma_s . n = 0)
+FUN6 = dot(ez, lam("+")) * V_0("+") * dS
+
+# ALE bulk equation
 FUN7 = -inner(sigma_a, grad(v_a)) * dx(fluid) + inner(lam_a("+"), v_a("+")) * dS
+
+# Continuity of fluid and solid displacement
 FUN8 = inner(avg(eta_a), u_a("+") - u_s("-")) * dS
 
+# mean axial solid displacement is zero
 FUN9 = dot(ez, u_s) * g_0 * dx(solid)
+
+# mean fluid pressure is zero
 FUN10 = p_f * eta_p * dx(fluid)
 
 
@@ -186,7 +310,7 @@ solver = BlockPETScSNESSolver(problem)
 solver.parameters.update(snes_solver_parameters["snes_solver"])
 
 # extract solution components
-(u_f, p_f, f_0, lam_p, u_s, p_s, lam, U_0, u_a, lam_a) = X.block_split()
+(u_f, p_f, u_s, p_s, f_0, U_0, lam, lam_p, u_a, lam_a) = X.block_split()
 
 """
     quantities for separate meshes
@@ -201,7 +325,7 @@ u_a_only = Function(Vf)
 u_s_only = Function(Vs)
 
 
-def save(n):
+def save(eps):
     u_f_only = project(u_f, Vf)
     u_a_only = project(u_a, Vf)
     u_s_only = project(u_s, Vs)
@@ -210,49 +334,20 @@ def save(n):
     u_a_only.rename("u_a", "u_a")
     u_s_only.rename("u_s", "u_s")
 
-    output_f.write(u_f_only, n)
-    output_f.write(u_a_only, n)
-    output_s.write(u_s_only, n)
+    output_f.write(u_f_only, eps)
+    output_f.write(u_a_only, eps)
+    output_s.write(u_s_only, eps)
 
 
-"""
-    BCs for the ALE problem
-"""
+#---------------------------------------------------------------------
+# Solve
+#---------------------------------------------------------------------
 
 
-# ac_inlet = DirichletBC(V_ALE.sub(0), Constant((0,0)), bdry, inlet)
-# ac_outlet = DirichletBC(V_ALE.sub(0), Constant((0,0)), bdry, outlet)
-# ac_fluid_axis = DirichletBC(V_ALE.sub(0).sub(1), Constant((0)), bdry, fluid_axis)
-# ac_wall = DirichletBC(V_ALE.sub(0).sub(1), Constant((0)), bdry, wall)
-# ac_int = DirichletBC(V_ALE.sub(0), u_s, bdry, circle)
-#
-#
-# bc_ALE = BlockDirichletBC([ac_inlet, ac_outlet, ac_fluid_axis, ac_wall, ac_int])
+# last converged value of epsilon
+eps_conv = 0
 
-# sigma_a = -p_a * I + grad(u_a) + grad(u_a).T
-# ic_a = Constant(1) * div(u_a)
-#
-# # F_a = I + grad(u_a)
-# # E_a = 1/2 * (F_a.T * F_a - I)
-# # sigma_a = -p_a * I + 2 * E_a
-# # ic_a = tr(E_a)
-#
-# F_ALE = [-inner(sigma_a, grad(v_a)) * dx(fluid), (p_a - ic_a) * q_a * dx(fluid)]
-# J_ALE = block_derivative(F_ALE, X_ALE, Xt_ALE)
-#
-# problem_ALE = BlockNonlinearProblem(F_ALE, X_ALE, bc_ALE, J_ALE)
-# solver_ALE = BlockPETScSNESSolver(problem_ALE)
-# solver_ALE.parameters.update(snes_solver_parameters["snes_solver"])
-#
-# (u_a, p_a) = X_ALE.block_split()
-
-
-"""
-    solve
-"""
-
-n = 0
-eps_try = eps_conv
+# increment epsilon and solve
 while eps_conv <= eps_max:
 
     print('-------------------------------------------------')
@@ -260,27 +355,40 @@ while eps_conv <= eps_max:
 
     (its, conv) = solver.solve()
 
-    # if converged
-    if conv == True:
-        save(n)
+    # if the solver converged...
+    if conv:
 
+        # update value of eps_conv and save
+        eps_conv = float(eps)
+        save(eps_conv)
+        
+        # if converence was fast, increase the
+        # increment in epsilon
         if its < 3 and de < de_max:
             de *= 1.01
 
-        eps_conv = float(eps)
-        eps_try = eps_conv + de
+        # update the value of epsilon to try to solve the problem with
+        eps_try += de
 
+        # copy the converged solution into old solution
         block_assign(X_old, X)
-        n += 1
 
-    if conv == False:
+
+
+    # if the solver diverged...
+    if not(conv):
+
+        # halve the increment in epsilon if not at smallest value
+        # and use the previously converged solution as the initial
+        # guess
         if de > de_min:
             de /= 2
             eps_try = eps_conv + de
             block_assign(X, X_old)
         else:
             print('min increment reached...aborting')
-            save(n)
+            save(eps_try)
             break
 
+    # update the value of epsilon
     eps.assign(eps_try)
